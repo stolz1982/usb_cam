bfce670efc0372970189181b3b002b4392f904ab
Steffen Stolz <stolz1982@gmx.de>
2018-08-10 14:48:46 +0200

    first uploaded pic but parameterization still missing

--- README.md
+++ README.md
@@ -2,5 +2,10 @@ Requirements:
 
 sudo apt-get install fswebcam git
 
+Download gdrive app
+https://docs.google.com/uc?id=0B3X9GlR6EmbnRjBaMVVLalN4cTA&export=download
+
+
+
 Command:
 fswebcam test.jpg
--- take_pic.py
+++ take_pic.py
@@ -1,3 +1,7 @@
 import os 
 
-os.system('fswebcam -r 320x240 --jpeg 50 --no-banner --quiet --save ./current.jpg') 
+GDRIVEID="1oc0Ccjtdd8Gd-wHkRuRHgiO294ubcAfd" #freigabe ./Germany/pictures
+FILENAME="CURRENT"
+os.system('fswebcam -r 320x240 --jpeg 50 --no-banner --quiet --save ./CURRENT.jpg')
+#os.system('./gdrive-linux-arm upload --parent $GDRIVEID $FILENAME')
+os.system('./gdrive-linux-arm upload --parent 1oc0Ccjtdd8Gd-wHkRuRHgiO294ubcAfd ./CURRENT.jpg')
