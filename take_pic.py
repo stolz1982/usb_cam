ad746b92d8496d9e5198aaf11377cb0305689662
stolz1982 <stolz1982@gmx.de>
2018-08-12 13:05:09 +0200

    Update take_pic.py

--- take_pic.py
+++ take_pic.py
@@ -1,7 +1,18 @@
 import os 
+import urllib2
 
-GDRIVEID="1oc0Ccjtdd8Gd-wHkRuRHgiO294ubcAfd" #freigabe ./Germany/pictures
-FILENAME="CURRENT"
-os.system('fswebcam -r 320x240 --jpeg 50 --no-banner --quiet --save ./CURRENT.jpg')
-#os.system('./gdrive-linux-arm upload --parent $GDRIVEID $FILENAME')
-os.system('./gdrive-linux-arm upload --parent 1oc0Ccjtdd8Gd-wHkRuRHgiO294ubcAfd ./CURRENT.jpg')
+def internet_on():
+    try:
+        urllib2.urlopen('http://8.8.8.8', timeout=1)
+        return True
+    except urllib2.URLError as err: 
+        return False
+
+if internet_on = true:
+  GDRIVEID="1oc0Ccjtdd8Gd-wHkRuRHgiO294ubcAfd" #freigabe ./Germany/pictures
+  FILENAME="CURRENT"
+  os.system('fswebcam -r 320x240 --jpeg 50 --no-banner --quiet --save ./CURRENT.jpg')
+  #os.system('./gdrive-linux-arm upload --parent $GDRIVEID $FILENAME')
+  os.system('./gdrive-linux-arm upload --parent 1oc0Ccjtdd8Gd-wHkRuRHgiO294ubcAfd ./CURRENT.jpg')
+  
+  
