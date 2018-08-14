cd88da483f32c02cf1dbf7ef0278ee06143d4d3e
Steffen Stolz <stolz1982@gmx.de>
2018-08-14 15:47:49 +0200

    pushed

--- take_pic.sh
+++ take_pic.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+
 GDRIVEID="1oc0Ccjtdd8Gd-wHkRuRHgiO294ubcAfd" #freigabe ./Germany/pictures
 ARCH=$(arch)
 NOW=$(date +"%YYYY%mm%dd-%H%M%S")
 FILENAME="CURRENT.jpg"
+BRIGHTNESS=5%
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root (with sudo)"
+  exit
+fi
 
 if [ "$ARCH" = "x86_64" ];  then
 	ARCH='x64'
@@ -10,7 +17,7 @@ else
 	ARCH='arm'
 fi
 
-fswebcam -r 1920x1080 --set brightness=20% --jpeg 50 --no-banner --quiet --save $FILENAME
+fswebcam -r 1920x1080 --set brightness=$BRIGHTNESS --jpeg 50 --no-banner --quiet --save $FILENAME
 ./gdrive-linux-$ARCH list --query "'$GDRIVEID' in parents" | grep $FILENAME | cut -d " " -f 1 | xargs -n1 ./gdrive-linux-$ARCH delete
 ./gdrive-linux-$ARCH upload --parent $GDRIVEID $FILENAME
   
